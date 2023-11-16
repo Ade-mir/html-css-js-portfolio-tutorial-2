b91d9b8a0a1d49a56d0a33205952c43fd1d024b0
Sanhita <sacred.profiles.02@gmail.com>
2023-11-16 17:26:46 +0530

    welcome page, navigation bar, committee logos added

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function autoScroll() {
+  // Set the delay (in milliseconds) after which you want to scroll
+  var scrollDelay = 1250; // Adjust this value as needed (5000 milliseconds = 5 seconds)
+
+  setTimeout(function() {
+    // Calculate the new scroll position or use any logic you need
+    var newScrollPosition = window.scrollY + window.innerHeight;
+
+    // Scroll to the new position
+    window.scrollTo({
+      top: newScrollPosition,
+      behavior: 'smooth' // Use smooth scrolling for a better user experience
+    });
+
+  }, scrollDelay);
+}
+
+// Call the autoScroll function when the page is loaded
+window.addEventListener('load', autoScroll);
+
+
+
