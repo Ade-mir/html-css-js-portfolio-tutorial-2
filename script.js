efb1ecb69ad3f12d29483364619071fe5e54241e
tweve09 <emmanueltweve09@gmail.com>
2024-01-19 00:02:50 +0300

    Update footer with current year

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// update footer with current year
+const current_year = new Date().getFullYear();
+document.getElementById("year").innerText = current_year
